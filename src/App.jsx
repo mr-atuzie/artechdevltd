5430cf8f61775a0445dc51ac06d0451229c4bde3
Rex Atuzie <rexatuzie@gmail.com>
2025-10-14 19:36:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>EXAM PREP GLOBAL</h1>
+
+      <p className="read-the-docs">Welcome to Exam Prep Global!</p>
+    </>
+  );
+}
+
+export default App;
