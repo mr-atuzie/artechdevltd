30c8bec6632a42bcb46684431e4f4229500a7a16
Rex Atuzie <rexatuzie@gmail.com>
2025-10-14 19:40:53 -0700

    welcome text

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>EXAM PREP GLOBAL</h1>
+      <h1>ARTECH DEV LTD</h1>
 
-      <p className="read-the-docs">Welcome to Exam Prep Global!</p>
+      <p className="read-the-docs">Development in progress, Artech is coming</p>
     </>
   );
 }
